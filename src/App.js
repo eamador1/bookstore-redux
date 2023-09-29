0d2e1037926582568e6398d2e4e343641b143366
Erwin Amador <eamador0305@gmail.com>
2023-09-29 11:38:23 -0600

    add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+import {
+  BrowserRouter, NavLink, Routes, Route,
+} from 'react-router-dom';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+
+export default function App() {
+  const activeClassName = 'nav-active';
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <section>
+        <nav>
+          <li>Bookstore CMS</li>
+          <NavLink to="" className={({ isActive }) => isActive && activeClassName}>Books</NavLink>
+          <NavLink to="categories" className={({ isActive }) => isActive && activeClassName}>Categories</NavLink>
+        </nav>
+
+      </section>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
