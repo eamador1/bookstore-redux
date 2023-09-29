6b0c1fbe33f9a87af5facd8da3001ed72c730f3a
Erwin Amador <eamador0305@gmail.com>
2023-09-29 11:35:44 -0600

    create component button type button

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button({ children }) {
+  return (
+    <button type="button">
+      { children }
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+export default Button;
