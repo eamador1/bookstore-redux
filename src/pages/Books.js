1ac8bde9633632a6ad519dc1a288a7c77acb88e9
Erwin Amador <eamador0305@gmail.com>
2023-09-29 11:41:52 -0600

    Create page named  book

---
+++ src/pages/Books.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BookList from '../components/BookList';
+import Form from '../components/Form';
+
+function Books() {
+  return (
+    <div>
+      <BookList />
+      <Form />
+    </div>
+  );
+}
+
+export default Books;
