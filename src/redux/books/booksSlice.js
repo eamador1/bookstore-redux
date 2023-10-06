bd0735561cb1e9a65b2ac05f077ffba137f899bf
Erwin Amador <eamador0305@gmail.com>
2023-10-06 16:09:20 -0600

    async book slice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,31 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { v4 as uuidv4 } from 'uuid';
-import bookItems from '../../bookItems';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getAllApiBooks, addBooktoAPI, deleteBookFromAPI } from '../../api/api';
 
-const initialState = {
-  bookItems,
-  isLoading: true,
-};
-
-const bookSlice = createSlice({
-  name: 'book',
-  initialState,
-  reducers: {
-    addBook: (state, action) => {
-      state.bookItems.push({
-        title: action.payload.title,
-        author: action.payload.author,
-        itemId: uuidv4(),
-      });
-    },
-    removeBook: (state, action) => ({
-      ...state,
-      bookItems: state.bookItems.filter((item) => item.itemId !== action.payload.itemId),
-    }),
-
-  },
-
+export const fetchBooks = createAsyncThunk('books/getBooks', async () => {
+  const response = await getAllApiBooks();
+  return response;
 });
 
-export const { addBook, removeBook } = bookSlice.actions;
-export default bookSlice.reducer;
+export const addBook = createAsyncThunk('books/addBook', async (book) => {
+  await addBooktoAPI(book);
+  return book;
+});
+
+export const removeBook = createAsyncThunk('books/removeBook', async (itemId) => {
+  await deleteBookFromAPI(itemId);
+  return itemId;
+});
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.fulfilled, (state, action) => action.payload)
+      .addCase(addBook.fulfilled, (state, action) => {
+        state.push(action.payload);
+      })
+      .addCase(removeBook.fulfilled, (state, action) => {
+        const index = state.findIndex((book) => book.item_id === action.payload);
+        if (index !== -1) {
+          state.splice(index, 1);
+        }
+      });
+  },
+});
+
+export default booksSlice.reducer;
