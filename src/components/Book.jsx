21699a0e9c4073ba39636e71cbdb196af824b6f9
Erwin Amador <eamador0305@gmail.com>
2023-10-06 16:08:16 -0600

    generate list of books

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import '../styles/book.css';
-import Button from './Button';
+import { removeBook } from '../redux/books/booksSlice';
 
-const Book = ({
-  title, author, itemId,
-}) => (
-  <div>
-    <h4>{ title }</h4>
-    <span>{ author }</span>
-    <Button itemId={itemId} />
-  </div>
-);
+const Book = ({ item }) => {
+  const dispatch = useDispatch();
 
+  const handleDelete = () => {
+    dispatch(removeBook(item.item_id));
+  };
+  return (
+    <div>
+      <h3>{item.title}</h3>
+      <p>
+        Author:
+        {' '}
+        {item.author}
+      </p>
+      <button type="button" onClick={handleDelete}>
+        Delete
+      </button>
+    </div>
+  );
+};
 Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  itemId: PropTypes.string.isRequired,
+  item: PropTypes.shape({
+    item_id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Book;
