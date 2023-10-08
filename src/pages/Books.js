8f4203289dcd7544da176b33bd49bb7e6a19d3f6
Erwin Amador <eamador0305@gmail.com>
2023-10-07 20:45:28 -0600

    import css file and add header to book page

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BookList from '../components/BookList';
 import Form from '../components/Form';
+import '../styles/pagebook.css';
 
 function Books() {
   return (
     <div>
       <BookList />
+      <h2 className="titleAddBook">ADD NEW BOOK</h2>
       <Form />
     </div>
   );
