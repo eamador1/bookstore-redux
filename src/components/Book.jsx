b15cc7a105058c75d087e5fb7064f08e29068404
Erwin Amador <eamador0305@gmail.com>
2023-10-07 20:40:39 -0600

    add dummy components to display with each book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -11,17 +11,53 @@ const Book = ({ item }) => {
     dispatch(removeBook(item.item_id));
   };
   return (
-    <div>
-      <h3>{item.title}</h3>
-      <p>
-        Author:
-        {' '}
-        {item.author}
-      </p>
-      <button type="button" onClick={handleDelete}>
-        Delete
-      </button>
-    </div>
+    <section className="contBookSurrounding">
+      <div className="contBook">
+        <span className="category">Action</span>
+        <h3 className="Title">{item.title}</h3>
+        <span className="Suzanne-Collins">
+          Author:
+          {' '}
+          {item.author}
+        </span>
+        <div className="allButtons">
+          <button className="CommentsButton" type="button">Comments</button>
+          <button className="RemoveButton" type="button" onClick={handleDelete}>
+            Remove
+          </button>
+          <button className="EditButton" type="button">Edit</button>
+        </div>
+      </div>
+      <div className="Advance">
+        <div className="contCircularBar">
+          <div className="circle-wrap">
+            <div className="circle">
+              <div className="mask half">
+                <div className="fill">
+                  <div className="mask full">
+                    <div className="inside-circle" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="textAdvance">
+          <span className="numericalAdvance">100%</span>
+          <span className="completed">Completed</span>
+        </div>
+      </div>
+      <div className="updateInformation">
+        <div className="divisionContainer">
+          <div className="verticalDivision" />
+        </div>
+        <div className="chapterUpdate">
+          <span className="currentChapter">CURRENT CHAPTER</span>
+          <span className="chapterNumber">CHAPTER 5</span>
+          <button className="updateButton" type="button">UPDATE PROGRESS</button>
+        </div>
+      </div>
+    </section>
   );
 };
 Book.propTypes = {
